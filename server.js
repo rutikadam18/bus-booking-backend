75fb6b9f1bf304d9d9776af0cc7d4f8be43ec259
demo <rutikadam@gmail.com>
2026-03-14 12:22:19 +0530

    Fixed port and MongoDB env variable

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB Atlas
-mongoose.connect("mongodb+srv://rutikadam18:Rucha18@cluster0.djht58u.mongodb.net/busBooking")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error("Connection error:", err));
 
@@ -49,4 +49,6 @@ app.delete("/book", async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+// Use Render's dynamic port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
